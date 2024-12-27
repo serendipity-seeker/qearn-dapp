3ff98bbc52e3ddb3318b55d71f435066fffc8544
serendipity-seeker <valkyrie.prim@gmail.com>
2024-12-27 18:53:01 +0900

    feat: extract rpc call

--- src/hooks/useFetchBalance.ts
+++ src/hooks/useFetchBalance.ts
@@ -1,16 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { Balance } from '@/types';
-import { httpEndpoint } from '@/constants';
-
-export const fetchBalance = async (publicId: string): Promise<Balance> => {
-  const balanceResult = await fetch(`${httpEndpoint}/v1/balances/${publicId}`);
-  const balance = await balanceResult.json();
-  if (!balance || !balance.balance) {
-    console.warn('getBalance: Invalid balance');
-    return {} as Balance;
-  }
-  return balance.balance;
-};
+import { fetchBalance } from '@/services/rpc.service';
 
 export const useFetchBalance = (publicId: string) => {
   return useQuery<Balance, Error>({
