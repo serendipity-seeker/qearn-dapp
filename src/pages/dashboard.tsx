0c099eeb90587b37fb59930b290461f08d94752a
serendipity-seeker <valkyrie.prim@gmail.com>
2025-01-04 22:08:59 +0900

    feat: test api

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,24 @@
+import { useEffect } from 'react';
+import { getLockInfoPerEpoch, getUserLockInfo, getStateOfRound, getUserLockStatus, getEndedStatus } from '@/services/qearn.service';
+import { PublicKey } from '@qubic-lib/qubic-ts-library/dist/qubic-types/PublicKey';
+
 const Dashboard: React.FC = () => {
-    return <div>Dashboard</div>;
+  const testAPIs = async () => {
+    const lockInfo = await getLockInfoPerEpoch(140);
+    const userLockInfo = await getUserLockInfo((new PublicKey('TLEIBEQEXQKJLBQXENQJZLKEZIGAKLVXYSTVDHYEAHRPFJPLFWROUWGBJNIB')).getPackageData(), 140);
+    const stateOfRound = await getStateOfRound(140);
+    const userLockStatus = await getUserLockStatus((new PublicKey('TLEIBEQEXQKJLBQXENQJZLKEZIGAKLVXYSTVDHYEAHRPFJPLFWROUWGBJNIB')).getPackageData(), 140);
+    const endedStatus = await getEndedStatus((new PublicKey('TLEIBEQEXQKJLBQXENQJZLKEZIGAKLVXYSTVDHYEAHRPFJPLFWROUWGBJNIB')).getPackageData());
+    console.log('Lock Info', lockInfo);
+    console.log('User Lock Info', userLockInfo);
+    console.log('State Of Round', stateOfRound);
+    console.log('User Lock Status', userLockStatus);
+    console.log('Ended Status', endedStatus);
   };
-  
-  export default Dashboard;
-  
+  useEffect(() => {
+    testAPIs();
+  }, []);
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
