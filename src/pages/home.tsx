42282eaccd5c56d1a38e126cc3a6084b38f92df1
serendipity-seeker <valkyrie.prim@gmail.com>
2025-01-06 17:40:36 +0900

    chore: remove unnecessary components

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,9 @@
-import AccountInfo from '@/components/AccountInfo';
 import QearnForm from '@/components/QearnForm';
 
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col gap-4 justify-center">
       <QearnForm />
-      <AccountInfo />
     </div>
   );
 };
