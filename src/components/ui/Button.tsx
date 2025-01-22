d1ec5c10efc4cc77e2c185576c3019425f5b4895
serendipity-seeker <valkyrie.prim@gmail.com>
2025-01-23 00:54:04 +0900

    ui: update button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -20,19 +20,15 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 /**
  * Button component with primary, secondary and tertiary variants
  */
-const Button = ({ variant = 'primary', label, className, icon, ...props }: ButtonProps) => {
+const Button: React.FC<ButtonProps> = ({ variant = 'primary', label, className, icon, ...props }) => {
   const variants = {
     primary: 'bg-primary-40 text-black hover:bg-primary-50',
-    secondary: 'bg-[rgba(26,222,245,0.1)] text-primary-40 hover:bg-[rgba(26,222,245,0.2)]',
-    tertiary: 'bg-transparent text-gray-50 hover:text-primary-40',
+    secondary: 'bg-[rgba(26,222,245,0.1)] text-primary-30 hover:bg-[rgba(26,222,245,0.2)]',
+    tertiary: 'bg-transparent text-primary-30 hover:text-primary-40',
   };
 
   return (
-    <button
-      type="button"
-      className={clsx('p-3 rounded-xl flex items-center justify-center border-none', 'hover:scale-[1.02] active:scale-[0.98] transition-transform', variants[variant], className)}
-      {...props}
-    >
+    <button type="button" className={clsx('p-3 rounded-xl flex items-center justify-center border-none', 'focus:ring', variants[variant], className)} {...props}>
       {icon}
       {label}
     </button>
