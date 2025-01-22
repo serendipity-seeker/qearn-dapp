da2644c70b9b6e5c3c1d5219604a70596aada2eb
serendipity-seeker <valkyrie.prim@gmail.com>
2025-01-22 20:27:02 +0900

    fix: build error

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,36 +1,8 @@
-import { useEffect, useMemo } from 'react';
-import { getLockInfoPerEpoch, getUserLockInfo, getStateOfRound, getUserLockStatus, getEndedStatus, getBurnedAndBoostedStats, getBurnedAndBoostedStatsPerEpoch } from '@/services/qearn.service';
-import { PublicKey } from '@qubic-lib/qubic-ts-library/dist/qubic-types/PublicKey';
-import LockStatCard from '@/components/LockStatCard';
-import { tickInfoAtom } from '@/store/tickInfo';
-import { useAtom } from 'jotai';
 import TVL from './charts/TVL';
 import QearnSummarize from './charts/QearnSummarize';
 import BonusAmountAnalyzer from './charts/BonusAmountAnalyzer';
 
 const Dashboard: React.FC = () => {
-  const [tickInfo] = useAtom(tickInfoAtom);
-  const currentEpoch = useMemo(() => tickInfo?.epoch || 142, [tickInfo?.epoch]);
-
-  const testAPIs = async () => {
-    const lockInfo = await getLockInfoPerEpoch(140);
-    const userLockInfo = await getUserLockInfo(new PublicKey('TLEIBEQEXQKJLBQXENQJZLKEZIGAKLVXYSTVDHYEAHRPFJPLFWROUWGBJNIB').getPackageData(), 140);
-    const stateOfRound = await getStateOfRound(140);
-    const userLockStatus = await getUserLockStatus(new PublicKey('TLEIBEQEXQKJLBQXENQJZLKEZIGAKLVXYSTVDHYEAHRPFJPLFWROUWGBJNIB').getPackageData(), 140);
-    const endedStatus = await getEndedStatus(new PublicKey('TLEIBEQEXQKJLBQXENQJZLKEZIGAKLVXYSTVDHYEAHRPFJPLFWROUWGBJNIB').getPackageData());
-
-    const burnedAndBoostedStats = await getBurnedAndBoostedStats();
-    const burnedAndBoostedStatsPerEpoch = await getBurnedAndBoostedStatsPerEpoch(138);
-
-    console.log('Lock Info', lockInfo);
-    console.log('User Lock Info', userLockInfo);
-    console.log('State Of Round', stateOfRound);
-    console.log('User Lock Status', userLockStatus);
-    console.log('Ended Status', endedStatus);
-    console.log('Burned And Boosted Stats', burnedAndBoostedStats);
-    console.log('Burned And Boosted Stats Per Epoch', burnedAndBoostedStatsPerEpoch);
-  };
-
   return (
     <div className="flex gap-4 flex-wrap">
       <QearnSummarize />
