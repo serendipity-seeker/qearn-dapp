888ae0f64fabdec0ab980737f03fbb009cd0752a
serendipity-seeker <valkyrie.prim@gmail.com>
2025-01-23 15:48:48 +0900

    feat: remove qearn summarize card

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,15 +1,14 @@
 import { motion } from 'framer-motion';
 import TVL from './charts/TVL';
-import QearnSummarize from './charts/QearnSummarize';
 import BonusAmountAnalyzer from './charts/BonusAmountAnalyzer';
 import Richlist from './charts/Richlist';
 
 const Dashboard: React.FC = () => {
   return (
     <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className="flex gap-4 flex-wrap">
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.2, duration: 0.5 }}>
+      {/* <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.2, duration: 0.5 }}>
         <QearnSummarize />
-      </motion.div>
+      </motion.div> */}
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.4, duration: 0.5 }}>
         <TVL />
       </motion.div>
