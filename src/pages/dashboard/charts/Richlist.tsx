5f2c420929f9b9f0dd1cc62112b94160a531977b
serendipity-seeker <valkyrie.prim@gmail.com>
2025-01-23 22:06:16 +0900

    chore: remove left barchart on richlist

--- src/pages/dashboard/charts/Richlist.tsx
+++ src/pages/dashboard/charts/Richlist.tsx
@@ -1,7 +1,7 @@
 import { EChart } from '@kbox-labs/react-echarts';
 import Card from '@/components/ui/Card';
 import { TitleComponent, TooltipComponent, LegendComponent, GridComponent } from 'echarts/components';
-import { BarChart, PieChart } from 'echarts/charts';
+import { PieChart } from 'echarts/charts';
 import { LabelLayout } from 'echarts/features';
 import { SVGRenderer } from 'echarts/renderers';
 import { EChartsOption } from 'echarts';
@@ -33,58 +33,20 @@ const Richlist: React.FC = () => {
   }, [latestStats]);
 
   const option: EChartsOption = {
-    title: [
-      {
-        text: 'RichList',
-        subtext: 'Top 10 Holders',
-        left: '25%',
-        textAlign: 'center',
-      },
-      {
-        text: 'Distribution Ratio',
-        left: '75%',
-        textAlign: 'center',
-      },
-    ],
+    title: {
+      text: 'Distribution Ratio',
+      left: 'center',
+      textAlign: 'center',
+    },
     tooltip: {
       trigger: 'item',
     },
-    grid: {
-      left: '5%',
-      right: '55%',
-      bottom: '3%',
-      containLabel: true,
-    },
-    xAxis: {
-      type: 'value',
-      splitLine: {
-        show: false,
-      },
-      show: false,
-    },
-    yAxis: {
-      type: 'category',
-      data: richlist.map((item) => item.identity),
-      axisLabel: {
-        interval: 0,
-        rotate: 30,
-      },
-    },
     series: [
-      {
-        name: 'Balance',
-        type: 'bar',
-        data: richlist.map((item) => item.balance),
-        label: {
-          show: true,
-          position: 'right',
-        },
-      },
       {
         name: 'Distribution',
         type: 'pie',
         radius: '60%',
-        center: ['75%', '50%'],
+        center: ['40%', '50%'],
         data: richlist.map((item) => ({
           value: item.balance,
           name: item.identity,
@@ -100,11 +62,11 @@ const Richlist: React.FC = () => {
     ],
   };
 
-  const chartComponents = [TitleComponent, TooltipComponent, LegendComponent, GridComponent, BarChart, PieChart, SVGRenderer, LabelLayout];
+  const chartComponents = [TitleComponent, TooltipComponent, LegendComponent, GridComponent, PieChart, SVGRenderer, LabelLayout];
 
   return (
-    <Card className="p-4">
-      <EChart style={{ width: '900px', height: '400px' }} theme={custom} use={chartComponents} {...option} />
+    <Card className="max-w-lg p-4">
+      <EChart style={{ width: '400px', height: '400px' }} theme={custom} use={chartComponents} {...option} />
     </Card>
   );
 };
