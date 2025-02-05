d67502f4d031787f02e5f4f87e045a921fc2f3af
serendipity-seeker <valkyrie.prim@gmail.com>
2025-02-05 14:41:09 +0900

    fix: responsive issue

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -20,7 +20,7 @@ const Home: React.FC = () => {
   }, [activeTab]);
 
   return (
-    <div className="mx-auto">
+    <div className={`mx-auto ${activeTab === 1 ? "w-full md:max-w-6xl" : ""}`}>
       <div className="relative mx-auto mb-4">
         <div className="pb-1">
           <span
